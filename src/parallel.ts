20d05a7c4dbd6843c259beb0b4a14c0c5d56f9c5
Serghei Gurgurov <serghei.gurgurov@gmail.com>
2024-04-06 00:01:01 +0200

    parallel  accepts any kind of value

--- src/parallel.ts
+++ src/parallel.ts
@@ -1,5 +1,25 @@
 export type FunctionWithCallback<T = any> = (...args: [...any, (data: T) => any]) => void;
-export function parallel(parallel: ($await: (_: void) => void, $: (data: any) => void) => void, complete: (results: any[]) => void) {
+/**
+ * this function will run all the functions in parallel and call the complete callback when all the functions are done,
+ * 
+ * the functions should call the $ function to signal that they are done
+ * 
+ * the results will NOT be necessarily in the same order as the functions were called
+ * 
+ * @example
+ * parallel(($await, $) => {
+ *   $await(setTimeout(() => {
+ *       $(1);
+ *   }, 1000));
+ *   $await(setTimeout(() => {
+ *       $(2);
+ *   }, 2000));
+ * },
+ * (results) => {
+ *   console.log(results); // [1, 2]
+ * });
+ */
+export function parallel(parallel: ($await: (_: unknown) => void, $: (data: any) => void) => void, complete: (results: any[]) => void) {
     let functionsToRun = 1;
 
     const results = [];
@@ -12,14 +32,16 @@ export function parallel(parallel: ($await: (_: void) => void, $: (data: any) =>
             complete(results);
         }
     };
-    const $await = (_: void) => {
+    const $await = (_: unknown) => {
         functionsToRun++;
     };
     parallel($await, $);
 
     $(undefined, true);
 }
-
+/**
+ * this function will returns a "proxy" function that will only call the callback once
+ */
 export function once<T extends unknown[]>(callback: (...args: T) => void) {
     const resolver = (function* () {
         yield true;
